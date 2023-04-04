1f822de543f426c4b9e5b5cd76681e4728c01088
leehojun0314 <dlghwns0314@naver.com>
2023-04-05 03:08:59 +0900

    test for progress

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	headers: () => {
+		return [
+			{
+				source: `${process.env.NEXT_PUBLIC_API_ENDPOINT}/message/v3`,
+				headers: [
+					{ key: 'Content-Type', value: 'application/json' },
+					{ key: 'Content-Length', value: '1024' },
+				],
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
