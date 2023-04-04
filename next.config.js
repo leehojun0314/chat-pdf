dc6452913289bdc4fc092fb81f86d2a230a75dc5
leehojun0314 <dlghwns0314@naver.com>
2023-04-05 03:23:46 +0900

    restoring nextconfig

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	headers: () => {
-		return [
-			{
-				source: `${process.env.NEXT_PUBLIC_API_ENDPOINT}/message/v3`,
-				headers: [
-					{ key: 'Content-Type', value: 'application/json' },
-					{ key: 'Content-Length', value: '1024' },
-				],
-			},
-		];
-	},
 };
 
 module.exports = nextConfig;
